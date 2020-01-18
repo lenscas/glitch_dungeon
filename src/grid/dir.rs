ceb9560ad5a93e5912baaf44f5970a5b29e08498
lenscas <lenscas@gmail.com>
2020-01-18 12:32:01 +0100

    Ran cargo fmt

--- src/grid/dir.rs
+++ src/grid/dir.rs
@@ -4,18 +4,18 @@ use rand::Rng;
 
 #[derive(Debug)]
 pub enum Dir {
-	Up,
-	Down,
-	Left,
-	Right,
+    Up,
+    Down,
+    Left,
+    Right,
 }
 impl Distribution<Dir> for Standard {
-	fn sample<R: Rng + ?Sized>(&self, rng: &mut R) -> Dir {
-		match rng.gen_range(0, 4) {
-			0 => Dir::Up,
-			1 => Dir::Down,
-			2 => Dir::Left,
-			_ => Dir::Right,
-		}
-	}
+    fn sample<R: Rng + ?Sized>(&self, rng: &mut R) -> Dir {
+        match rng.gen_range(0, 4) {
+            0 => Dir::Up,
+            1 => Dir::Down,
+            2 => Dir::Left,
+            _ => Dir::Right,
+        }
+    }
 }
--- src/grid/tile.rs
+++ src/grid/tile.rs
@@ -1,6 +1,6 @@
 #[derive(Clone, Debug)]
 pub struct Tile {
-	pub is_start: bool,
-	pub is_end: bool,
-	pub can_move: bool,
+    pub is_start: bool,
+    pub is_end: bool,
+    pub can_move: bool,
 }
