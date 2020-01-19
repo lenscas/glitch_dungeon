cd1dcf68e85d28e9547f29df855949e5d35d7941
lenscas <lenscas@gmail.com>
2020-01-19 09:09:35 +0100

    Fix too many renders

--- src/monster.rs
+++ src/monster.rs
@@ -15,6 +15,7 @@ pub struct Monster {
 	pub damage: isize,
 	pub speed: f32,
 	pub rendered_health: Image,
+	pub damage_cooldown: usize,
 }
 impl Monster {
 	pub fn new(location: Vector, font: &Font, style: &FontStyle) -> Result<Self> {
@@ -29,14 +30,22 @@ impl Monster {
 			speed: 5.,
 			started_negative: health < 0,
 			rendered_health,
+			damage_cooldown: 0,
 		})
 	}
 	pub fn move_a_bit(&mut self, grid: &Grid) {
+		if self.damage_cooldown > 0 {
+			self.damage_cooldown -= 1;
+		}
 		let mut rng = rand::thread_rng();
 		self.location
 			.move_some(rng.gen(), self.speed, grid, self.size);
 	}
 	pub fn get_damage(&mut self, damage: isize, font: &Font, style: &FontStyle) -> Result<bool> {
+		if self.damage_cooldown > 0 {
+			return Ok(self.is_alive());
+		}
+		self.damage_cooldown = 20;
 		self.health -= damage;
 		self.rendered_health = font.render(&self.health.to_string(), style)?;
 		Ok(self.is_alive())
